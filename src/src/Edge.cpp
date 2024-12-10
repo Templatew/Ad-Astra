596b419461db3b8bbe4612778fb3932552eb3294
Cazaubon Lorenz <96289463+Templatew@users.noreply.github.com>
2024-12-10 21:36:13 +0100

    Delete src directory

--- src/src/Edge.cpp
+++
@@ -1,4 +0,0 @@
-#include "Edge.hpp"
-
-Edge::Edge(Vertex* source, Vertex* destination, double length, const std::string& name)
-    : source(source), destination(destination), length(length), name(name) {}
